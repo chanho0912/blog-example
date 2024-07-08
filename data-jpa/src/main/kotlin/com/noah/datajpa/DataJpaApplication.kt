a67696885cc2fd093ce02b608ecb17d44bc136b4
chanho0912-kim <chanho0912.kim@linecorp.com>
2024-07-08 19:26:39 +0900

    first commit

---
+++ data-jpa/src/main/kotlin/com/noah/datajpa/DataJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.noah.datajpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<DataJpaApplication>(*args)
+}
