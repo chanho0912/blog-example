d3c3b90bfdcb4e0119d43f2d0419f59e7df6ab90
chanho0912-kim <chanho0912.kim@linecorp.com>
2024-07-22 23:57:43 +0900

    blog 예제 추가 + test

--- spring-data/src/test/kotlin/com/noah/springdata/blog/service/SimpleJDBCServiceTest.kt
+++ spring-data/src/test/kotlin/com/noah/springdata/blog/service/SimpleJDBCServiceTest.kt
@@ -2,16 +2,20 @@ package com.noah.springdata.blog.service
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kotlin.random.Random
 
 class SimpleJDBCServiceTest : FunSpec({
 
     val service = SimpleJDBCService()
+    val testName = "blog-example"
+    val testId = Random(10).nextInt()
+
     test("insert") {
-        service.insert(10, "blog-example")
+        service.insert(testId, testName)
     }
 
     test("find") {
-        val saved = service.findById(10)
-        saved.second shouldBe "blog-example"
+        val saved = service.findById(testId)
+        saved.second shouldBe testName
     }
 })
