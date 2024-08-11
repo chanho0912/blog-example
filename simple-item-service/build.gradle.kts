0d62c19753bf14f5161efbe83f672020de5aff50
chanho0912-kim <chanho0912.kim@linecorp.com>
2024-08-11 21:33:58 +0900

    Set QueryDsl

--- simple-item-service/build.gradle.kts
+++ simple-item-service/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("kotlin-conventions")
     id("testing-conventions")
     id("spring-conventions")
+
+    kotlin("kapt")
 }
 
 repositories {
@@ -20,6 +22,17 @@ dependencies {
 
     // JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // querydsl
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    implementation("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    implementation("jakarta.persistence:jakarta.persistence-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    // https://mvnrepository.com/artifact/org.jetbrains/annotations
+    implementation("org.jetbrains:annotations:24.1.0")
+
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
