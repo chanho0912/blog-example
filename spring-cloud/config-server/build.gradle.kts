909103c3da6d145fe307bec048285ba6404fade2
chanho0912-kim <chanho0912.kim@linecorp.com>
2024-08-14 19:14:50 +0900

    Add Config server Security

--- spring-cloud/config-server/build.gradle.kts
+++ spring-cloud/config-server/build.gradle.kts
@@ -12,6 +12,17 @@ dependencies {
     // https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-config-server
     implementation("org.springframework.cloud:spring-cloud-config-server:4.1.3")
 
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.0")
+
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.0")
+
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-security
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.0")
+
+//    // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt
+//    implementation("io.jsonwebtoken:jjwt:0.12.0")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
